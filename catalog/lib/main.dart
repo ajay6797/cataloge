1a94db5467ffef462009e4b8cc057831fcaada0e
ajay6797 <ajayterani67@gmail.com>
2023-04-20 14:28:38 +0530

    day 3 completed

--- catalog/lib/main.dart
+++ catalog/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:catalog/hom_page.dart';
+import 'package:catalog/pages/hom_page.dart';
+import 'package:catalog/pages/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: homePage(),
+      // home: homePage(),
+      theme: ThemeData(primarySwatch: Colors.amber),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/Login": (context) => LoginPage()
+      },
     );
   }
 }
